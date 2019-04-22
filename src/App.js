091fa77b17fbd3cd8655d7337418d8f929d5d7c5
unknown <chris.a.desilva@gmail.com>
2019-04-21 19:02:04 -0500

    Update home page title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <h1>Challenge Tracker</h1>
       </div>
     );
   }
